02e26693cb480e1b343881101af7d30391a8c66f
sungtaekpark <sungtaek.park@onepredict.com>
2021-01-13 12:27:31 +0900

    Feat: Add Highcharts Sample - 하이차트 샘플을 추가했습니다

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+// highcharts-react module 불러오기
+import * as Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 
 function App() {
+  const initialOptions: any = {
+    title: {
+      text: 'Solar Employment Growth by Sector, 2010-2016',
+    },
+
+    subtitle: {
+      text: 'Source: thesolarfoundation.com',
+    },
+
+    yAxis: {
+      title: {
+        text: 'Number of Employees',
+      },
+    },
+
+    xAxis: {
+      accessibility: {
+        rangeDescription: 'Range: 2010 to 2017',
+      },
+    },
+
+    legend: {
+      layout: 'vertical',
+      align: 'right',
+      verticalAlign: 'middle',
+    },
+
+    plotOptions: {
+      series: {
+        label: {
+          connectorAllowed: false,
+        },
+        pointStart: 2010,
+      },
+    },
+
+    series: [
+      {
+        name: 'Installation',
+        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
+      },
+      {
+        name: 'Manufacturing',
+        data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434],
+      },
+      {
+        name: 'Sales & Distribution',
+        data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387],
+      },
+      {
+        name: 'Project Development',
+        data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227],
+      },
+      {
+        name: 'Other',
+        data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111],
+      },
+    ],
+
+    responsive: {
+      rules: [
+        {
+          condition: {
+            maxWidth: 500,
+          },
+          chartOptions: {
+            legend: {
+              layout: 'horizontal',
+              align: 'center',
+              verticalAlign: 'bottom',
+            },
+          },
+        },
+      ],
+    },
+  };
+  const [options, setOptions] = useState<any>(initialOptions);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HighchartsReact highcharts={Highcharts} options={options} />
     </div>
   );
 }
